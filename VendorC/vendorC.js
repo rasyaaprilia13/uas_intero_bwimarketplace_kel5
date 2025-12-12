fc1f370490095d318aa53f034d0a3ccecc6d5709
maulinadwi <maulinadwi04@gmail.com>
2025-12-12 13:43:59 +0700

    Tambah data resto & kuliner

---
+++ VendorC/vendorC.js
@@ -0,0 +1,16 @@
+const vendorCData = [
+  {
+    "id": 501,
+    "details": {
+      "name": "Nasi Tempong",
+      "category": "Food"
+    },
+    "pricing": {
+      "base_price": 20000,
+      "tax": 2000
+    },
+    "stock": 50
+  }
+];
+
+module.exports = vendorCData;
--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 //const vendorAData = require('./VendorA/vendorA.js');
 //console.log(vendorAData);
 
-const vendorBData = require('./VendorB/vendorB.js');
-console.log(vendorBData);
+//const vendorBData = require('./VendorB/vendorB.js');
+//console.log(vendorBData);
+
+const vendorCData = require('./VendorC/vendorC.js');
+console.log(vendorCData);
