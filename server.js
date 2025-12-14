fca344842da447a53fab8016be20fde0d50bce38
NAZWA RIDEV <ramadnazwa759@gmail.com>
2025-12-14 16:34:25 +0700

    Import data vendor

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3100;
 
+const vendorA = require("./VendorA/vendorA");
+const vendorB = require("./VendorB/vendorB");
+const vendorC = require("./VendorC/vendorC");
 
 
 app.listen(port, () => {
